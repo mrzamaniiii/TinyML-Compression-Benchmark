07f3ad7e46888ce2cc5775ac312b4ca4edc7e5c1
Mohammadreza Zamani <mrzamani.new@gmail.com>
2026-08-12 10:22:30 +0330

    Add files via upload

---
+++ Arduino/depthwise_cnn_scaler.h
@@ -0,0 +1,24 @@
+#ifndef DEPTHWISE_CNN_SCALER_H
+#define DEPTHWISE_CNN_SCALER_H
+
+constexpr int kRawChannels = 6;
+
+const float raw_mean[kRawChannels] = {
+    1.1208049254e-03f,
+    -8.2654111087e-02f,
+    -1.2643310428e-01f,
+    -1.5734744072e+00f,
+    1.5375390649e-01f,
+    2.9320248961e-01f
+};
+
+const float raw_std[kRawChannels] = {
+    2.6098391414e-01f,
+    6.0880005360e-01f,
+    7.4689579010e-01f,
+    1.9054401398e+01f,
+    1.7840723038e+01f,
+    5.6507167816e+01f
+};
+
+#endif
